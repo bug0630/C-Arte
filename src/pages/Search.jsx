69b54d7399a5f5af813409d50356a422d3bffb9d
bug0630 <bugbdh@gmail.com>
2024-06-30 19:03:12 +0900

    검색 쿼리받기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { setSearchQuery } from '../store/action';
 import Available from '../components/Search/Available';
 import SearchTab from '../components/Search/SearchTab';
 import Result from '../components/Search/Result';
@@ -7,15 +10,24 @@ import Footer from '../components/footer/Footer';
 import '../styles/Search.scss';
 
 export default function Search() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const query = queryParams.get('query');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setSearchQuery(query)); // 검색어 업데이트
+  }, [query, dispatch]);
+
   return (
     <div>
       <Header />
       <div className="searchLab">
         <SearchTab />
-        <Available />
-        <Result />
+        <Available></Available>
+        <Result></Result>
       </div>
-      <Footer />
+      <Footer></Footer>
     </div>
   );
 }
